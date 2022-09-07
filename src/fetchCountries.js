3fb5d58736aa413f2ff5ab9813d2fa04a866b21d
Darymee <danieledaniele3005@gmail.com>
2022-09-07 22:02:14 +0200

    update function FetchCountry with async

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,27 @@
-export function fetchContries(name) {
+export { fetchContries };
+
+// export function fetchContries(name) {
+//   const url = 'https://restcountries.com/v3.1/name/';
+//   const parametrs = '?fields=name,capital,flags,languages,population';
+//   const adress = `${url}${name}${parametrs}`;
+//   return fetch(adress).then(responce => {
+//     if (!responce.ok) {
+//       throw new Error(responce.status);
+//     }
+//     return responce.json();
+//   });
+// }
+
+async function fetchContries(name) {
   const url = 'https://restcountries.com/v3.1/name/';
   const parametrs = '?fields=name,capital,flags,languages,population';
   const adress = `${url}${name}${parametrs}`;
-  return fetch(adress).then(responce => {
-    if (!responce.ok) {
-      throw new Error(responce.status);
-    }
-    return responce.json();
-  });
+  try {
+    const response = await fetch(adress);
+    const country = response.json();
+    console.log(country);
+    return country;
+  } catch (error) {
+    console.log(error);
+  }
 }
